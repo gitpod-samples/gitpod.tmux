65ba2d15dd9cb74c99a5bc52565403619cbd2ec9
Danilo Tenorio Raisi <danilo@raisi.tech>
2023-12-15 12:15:19 +0000

    fix: Fix validate_gitpodyml
    Fix validate_gitpodyml to run only when connected to Gitpod CDE.

--- src/misc/validate_gitpodyml.sh
+++ src/misc/validate_gitpodyml.sh
@@ -1,23 +1,25 @@
 function misc::validate_gitpodyml() {
-    local run_prompt+=(
-        tmux display-menu
-        -T "#[align=centre fg=orange]Debug .gitpod.yml"
-        -x C -y C
-        ""
-            "-#[nodim, fg=green]Do you want to validate the workspace configuration?" "" ""
-        ""
+    if is::gitpod; then {
+        local run_prompt+=(
+            tmux display-menu
+            -T "#[align=centre fg=orange]Debug .gitpod.yml"
+            -x C -y C
+            ""
+                "-#[nodim, fg=green]Do you want to validate the workspace configuration?" "" ""
+            ""
 
-        "Validate" v "neww -n 'validate' 'gp validate'"
-        ""
-        "Dismiss" q ""
-    )
+            "Validate" v "neww -n 'validate' 'gp validate'"
+            ""
+            "Dismiss" q ""
+        )
 
-    function watch() {
-        tail -n 0 -F "${GITPOD_REPO_ROOT}/.gitpod.yml" 2>/dev/null | while read -r line; do
-            "${run_prompt[@]}";
-            break;
-        done || true;
+        function watch() {
+            tail -n 0 -F "${GITPOD_REPO_ROOT}/.gitpod.yml" 2>/dev/null | while read -r line; do
+                "${run_prompt[@]}";
+                break;
+            done || true;
+            watch;
+        }
         watch;
-    }
-    watch;
+    }; fi
 }
